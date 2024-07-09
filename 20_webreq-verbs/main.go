323cda7dfbcef280f64e090a339e0195b51a6e5a
uday kumar <uday33379@gmail.com>
2024-07-09 13:56:15 +0530

    GET POST POSTFORM Requests

--- 20_webreq-verbs/main.go
+++ 20_webreq-verbs/main.go
@@ -4,33 +4,68 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
+	"net/url"
+	
 )
 
 func main() {
 	fmt.Println("welcome to webrequests and verbs in Golang")
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
-func PerformGetRequest() {
-	const myurl = "https://api.freeapi.app/api/v1/public/randomjokes"
+// func PerformGetRequest() {
+// 	const myurl = "https://api.freeapi.app/api/v1/public/randomjokes"
+// 	response, err := http.Get(myurl)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer response.Body.Close()
+// 	fmt.Println("status code :",response.StatusCode)
+// 	fmt.Println("content length is : ",response.ContentLength)
+// 	//create a builder
+// 	var responseString strings.Builder
+// 	content,err:=io.ReadAll(response.Body)
+// 	bytecount,_ := responseString.Write(content)
+// 	fmt.Println("bytecount is : ",bytecount)
+// 	fmt.Println(responseString.String())
+// 	// fmt.Println(string(content))
+// }
 
-	response, err := http.Get(myurl)
+func PerformPostJsonRequest() {
+	// const myurl = "http://localhost:8000/post"
+	// //fake json payload
+	// requestBody := strings.NewReader(`
+	// {
+	//  "coursename":"lets go with golang",
+	//  "price":"0",
+	//  "platform":"learncodeonline.in
+	//  "
+	// }
+	// `)
+	// response,err:=http.Post(myurl,"application/json",requestBody)
+	// if err != nil {
+	// 	panic(err)	
+	// }
+	// defer response.Body.Close()
+	// io.ReadAll(response.Body)
+}
+
+func PerformGPostFormRequest(){
+	const myurl = "http:/localhost:8000/postform"
+
+	data := url.Values{}
+
+	data.Add("firstname","uday")
+	data.Add("lastname","kumar")
+	data.Add("email","uday@gmail.com")
+
+
+	res,err:= http.PostForm(myurl,data)
 	if err != nil {
 		panic(err)
 	}
-
-	
-	defer response.Body.Close()
-	fmt.Println("status code :",response.StatusCode)
-	fmt.Println("content length is : ",response.ContentLength)
-	//create a builder
-	var responseString strings.Builder
-	content,err:=io.ReadAll(response.Body)
-	bytecount,_ := responseString.Write(content)
-
-	fmt.Println("bytecount is : ",bytecount)
-	fmt.Println(responseString.String())
-
-	// fmt.Println(string(content))
+	defer res.Body.Close()
+	content ,_:= io.ReadAll((res.Body))
+	fmt.Println(string(content))
 }
