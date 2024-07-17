37d97296a32a985f39b7a929e38bff5ff863b2ef
uday kumar <uday33379@gmail.com>
2024-07-17 14:16:05 +0530

    Added Notes and reference code

--- 08_Slices/main.go
+++ 08_Slices/main.go
@@ -43,4 +43,39 @@ func main() {
 	courses=append(courses[:index] , courses[index+1])
 	fmt.Println(courses)
 
+
+	//Creatin slice
+	// Using a slice literal
+   slice := []int{1, 2, 3, 4, 5}
+
+// Using make to create a slice with a specific length and capacity
+
+// slice := make([]int, 5) 
+// Creates a slice with length 5 and capacity 5
+
+// var slice []int  // nil slice
+// slice = make([]int, 0)
+
+
+// Slicing Operations
+
+subSlice := slice[1:3]  // subSlice will be [2, 3]
+fmt.Println(subSlice)
+
+
+//Modifying Slice
+slice[0] = 10  // Modify an element
+slice = append(slice, 4, 5)  // Append elements to slice
+
+//length and capacity
+fmt.Println(len(slice))  // prints 5
+fmt.Println(cap(slice))  // prints 5
+
+// Iterating over slices
+for index, value := range slice {
+    fmt.Printf("Index: %d, Value: %d\n", index, value)
+}
+
+
+
 }
--- 09_maps/main.go
+++ 09_maps/main.go
@@ -1,25 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("maps in go lang")
-	languages:=make(map[string]string)
-	languages["js"]="javascript"
-	languages["py"]="python"
-	languages["rb"]="ruby"
+	languages := make(map[string]string)
+	languages["js"] = "javascript"
+	languages["py"] = "python"
+	languages["rb"] = "ruby"
 
 	fmt.Println("list of all loanguages ", languages)
 	fmt.Println("Js shorts for:  ", languages["js"])
-	delete(languages,"rb")
+	delete(languages, "rb")
 	fmt.Println("list of all loanguages ", languages)
 
-
-	//loops in maps are intresting
-	for key,value:= range languages{
-		fmt.Printf("For key %v, value is %v \n", key,value)
+	//loops in maps 
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v \n", key, value)
 	}
-	for _,value:= range languages{
+	for _, value := range languages {
 		fmt.Printf("For key v, value is %v \n", value)
 	}
+
+	//Creating and initializing a map
+	fruits := map[string]string{
+		"apple":  "1",
+		"banana": "2",
+	}
+
+	//adding an element
+	fruits["cherry"] = "3"
+
+	//updating an element
+	fruits["apple"] = "10"
+
+	//Accessing elements
+	fmt.Println("apple:", fruits["apple"])
+	fmt.Println("banana:", fruits["banana"])
+
+	//check if a key exists
+	value, ok := fruits["apple"]
+	if ok {
+		fmt.Println("Apple exists with value:", value)
+	} else {
+		fmt.Println("apple does no exist")
+	}
+
+	//Deleting an element
+	delete(fruits, "banana")
+
+	// Iterating over a map
+	for key, value := range fruits {
+		fmt.Fprintf(os.Stdout, "Key: %s, Value: %d\n", []any{key, value}...)
+	}
+
+
+	
+
+
+
 }
