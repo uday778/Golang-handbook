d0f447ac1d806b2e989421765e58c7e9735be1bc
uday kumar <uday33379@gmail.com>
2024-07-22 01:03:23 +0530

    rand pkg and random number generation

---
+++ 28_myMath/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto"
+	"fmt"
+	"math/big"
+	"math/rand"
+
+	// "math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("maths in golang")
+
+
+	// var mynum  int =2
+	// var mynum2 float64 =10.5
+	// fmt.Println("the sum is :",mynum + int(mynum2))// we loose precision
+
+
+	//random numbers 
+	//  rand.New(rand.NewSource(time.Now().UnixNano()))
+	// fmt.Println(rand.Intn(5)+1)
+
+
+
+
+}
